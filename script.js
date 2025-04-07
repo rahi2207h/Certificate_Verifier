e8bf06a729550908c0a2c9ff3f0fce00e375687b
rahi2207h <165833541+rahi2207h@users.noreply.github.com>
2025-04-08 00:11:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+        const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTlM8JaVWKQndSOis2MxBLPqpPxzV6F3x1E39SgnpdATm0FAVijTvMA20A_8hVk0TE1aRuXBOy2PZGp/pub?gid=0&single=true&output=csv";
+
+        async function verifyCertificate() {
+            const certNumber = document.getElementById("certNumber").value.trim();
+            const resultDiv = document.getElementById("result");
+
+            if (!certNumber) {
+                resultDiv.innerHTML = "<p style='color: #ff4d4d;'>⚠️ Please enter a certificate number.</p>";
+                return;
+            }
+
+            try {
+                const response = await fetch(sheetURL); //verification
+                const data = await response.text();
+                const certificateNumbers = data.split("\n").map(row => row.trim());
+
+                if (certificateNumbers.includes(certNumber)) {
+                    resultDiv.innerHTML = "<p style='color: #00ff99;'>✅ Valid Certificate <br> <span>Certified Learner</span></p>";
+                } else {
+                    resultDiv.innerHTML = "<p style='color: #ff4d4d;'>Incorrect detaild</p>"
+                }
+            } catch (error) {
+                resultDiv.innerHTML = "<p style='color: #ff4d4d;'>Error in loading</p>"
+            }
+        }
