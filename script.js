e9858e6b75c9a0be60bbbd28a5f46fe8a3f90f12
rahi2207h <165833541+rahi2207h@users.noreply.github.com>
2025-04-08 00:22:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTlM8JaVWKQndSOis2MxBLPqpPxzV6F3x1E39SgnpdATm0FAVijTvMA20A_8hVk0TE1aRuXBOy2PZGp/pub?gid=0&single=true&output=csv";
+       const sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTlM8JaVWKQndSOis2MxBLPqpPxzV6F3x1E39SgnpdATm0FAVijTvMA20A_8hVk0TE1aRuXBOy2PZGp/pub?gid=0&single=true&output=csv";
 
         async function verifyCertificate() {
             const certNumber = document.getElementById("certNumber").value.trim();
